46cabb7dcdaacdd318603096e643d24d398198b5
Kacper Tryniecki <mohairapp@hotmail.com>
2018-07-30 18:38:13 +0200

    test

--- Surrender@20.Core/ViewModel/MainPageViewModel.cs
+++ Surrender@20.Core/ViewModel/MainPageViewModel.cs
@@ -9,5 +9,17 @@ namespace Surrender_20.Core.ViewModel
     public class MainPageViewModel
     {
         public string Title { get; set; } = "Weź coś zrób";
-    }
+
+        public DelegateCommand MenuItemTapped { get; set; }
+
+        public void InitCommands()
+        {
+            MenuItemTapped = new DelegateCommand(MenuItemTapped);
+        }
+
+        private void MenuItemTapped()
+        {
+            //do something when clicked
+        }
+    }  
 }
