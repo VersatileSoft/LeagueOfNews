3c7c1742fbc22bbcac013deef79286363a36e521
Michał Woźniak <michalwozniak46@gmail.com>
2018-07-31 00:06:08 +0200

    Added xamarin forms android project

---
+++ Surrender@20.Forms/Surrender@20.Forms/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Surrender_20.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Surrender@20.Forms.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();
+
+        }
+    }
+}
