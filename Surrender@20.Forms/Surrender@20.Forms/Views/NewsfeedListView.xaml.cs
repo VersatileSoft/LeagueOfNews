a02974c8c258966a6e28bac5a0eccd4a6612b279
Michał Woźniak <michalwozniak46@gmail.com>
2018-10-30 18:57:47 +0100

    Tabbed page init

--- Surrender@20.Forms/Surrender@20.Forms/Views/NewsfeedListView.xaml.cs
+++ Surrender@20.Forms/Surrender@20.Forms/Views/NewsfeedListView.xaml.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms.Xaml;
 namespace Surrender_20.Forms.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    [MvxMasterDetailPagePresentation(MasterDetailPosition.Detail, NoHistory = true)]
+    [MvxTabbedPagePresentation(TabbedPosition.Tab, NoHistory = true)]
     public partial class NewsfeedListView : MvxContentPage<NewsfeedListViewModel>
     {
 		public NewsfeedListView()
@@ -17,8 +17,7 @@ namespace Surrender_20.Forms.Views
 		}
 
         private void MvxListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-            
+        {          
             ViewModel.ItemSelected.Execute((Newsfeed)e.SelectedItem);
         }
     }
--- Surrender@20.Forms/Surrender@20.Forms/Views/RootPage.xaml.cs
+++ Surrender@20.Forms/Surrender@20.Forms/Views/RootPage.xaml.cs
@@ -2,16 +2,19 @@
 using MvvmCross.Forms.Views;
 using Surrender_20.Forms.ViewModels;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.Xaml;
 
 namespace Surrender_20.Forms.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    [MvxMasterDetailPagePresentation(MasterDetailPosition.Root, WrapInNavigationPage = false)]
-    public partial class RootPage : MvxMasterDetailPage<RootViewModel>
+    [MvxTabbedPagePresentation(TabbedPosition.Root, WrapInNavigationPage = false)]
+    public partial class RootPage : MvxTabbedPage<RootViewModel>
     {
         public RootPage()
         {
             InitializeComponent();
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
         }
     }
 }
