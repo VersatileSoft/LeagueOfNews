40350f982a27d88dc01ae006bef86efb01dde97b
Michał Woźniak <michalwozniak46@gmail.com>
2019-01-09 23:17:32 +0100

    List of menu items.

--- Surrender@20.Forms/Surrender@20.Forms/ViewModels/MasterViewModel.cs
+++ Surrender@20.Forms/Surrender@20.Forms/ViewModels/MasterViewModel.cs
@@ -4,6 +4,7 @@ using PropertyChanged;
 using Surrender_20.Core.Interface;
 using Surrender_20.Forms.Interfaces;
 using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace Surrender_20.Forms.ViewModels
@@ -15,12 +16,24 @@ namespace Surrender_20.Forms.ViewModels
 
         public ICommand NavigateCommand { get; set; }
 
+        public ObservableCollection<MenuListElement> MenuElements { get; set; }
+
         public MasterViewModel(IMasterDetailService masterDetailService)
         {
-            NavigateCommand = new MvxCommand<string>((Parameter) =>
+
+            MenuElements.Add(new MenuListElement { Name = "SurrenderAt20", Page = Pages.SurrenderHome });
+            MenuElements.Add(new MenuListElement { Name = "LOL Official", Page = Pages.Official });
+
+            NavigateCommand = new MvxCommand<MenuListElement>((Parameter) =>
             {
-                masterDetailService.MasterPageSelect((Pages)Enum.Parse(typeof(Pages), Parameter));
+                masterDetailService.MasterPageSelect(Parameter.Page);
             });
         }
+    }
+
+    public class MenuListElement
+    {
+        public Pages Page { get; set; }
+        public string Name { get; set; }
     }
 }
