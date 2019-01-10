e3ef1d6084ecf6c3ae65b318ba6e29372ff77aeb
Kacper Tryniecki <kacper@tryniecki.com>
2019-01-10 16:01:12 +0100

    New look for app - dark theme and better hamburger menu

--- Surrender@20.Forms/Surrender@20.Forms/ViewModels/NoCategoryViewModels/NewsfeedListViewModel.cs
+++ Surrender@20.Forms/Surrender@20.Forms/ViewModels/NoCategoryViewModels/NewsfeedListViewModel.cs
@@ -3,7 +3,6 @@ using MvvmCross.ViewModels;
 using Surrender_20.Core.Interface;
 using Surrender_20.Core.ViewModels;
 using Surrender_20.Model;
-using System;
 using System.Threading.Tasks;
 
 namespace Surrender_20.Forms.ViewModels
