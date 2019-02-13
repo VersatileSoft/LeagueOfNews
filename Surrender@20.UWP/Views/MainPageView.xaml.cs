ae9c98a74ba310238570586129f68779b8a82acb
Kacper Tryniecki <kacper@tryniecki.com>
2019-02-13 18:36:51 +0100

    [UWP] Dialog -> Flyout

--- Surrender@20.UWP/Views/MainPageView.xaml.cs
+++ Surrender@20.UWP/Views/MainPageView.xaml.cs
@@ -3,15 +3,14 @@ using MvvmCross.Platforms.Uap.Views;
 using MvvmCross.ViewModels;
 using Surrender_20.UWP.Views.MessageBoxes;
 using System;
-using System.Net.NetworkInformation;
 using Windows.ApplicationModel.Core;
+using Windows.System;
 using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Media.Imaging;
-using Windows.UI.Xaml.Navigation;
 
 namespace Surrender_20.UWP.View
 {
@@ -19,12 +18,13 @@ namespace Surrender_20.UWP.View
     {
         private BitmapImage LogoLight, LogoDark;
         private readonly ConnectionDialog ConnectionDialog = new ConnectionDialog();
-        private readonly SiteChooseDialog SiteChooseDialog = new SiteChooseDialog();
 
         private IMvxInteraction<Func<bool>> _checkInternetConnectionInteraction;
-        public IMvxInteraction<Func<bool>> CheckInternetConnectionInteraction {
+        public IMvxInteraction<Func<bool>> CheckInternetConnectionInteraction
+        {
             get => _checkInternetConnectionInteraction;
-            set {
+            set
+            {
                 if (_checkInternetConnectionInteraction != null)
                     _checkInternetConnectionInteraction.Requested -= OnInternetCheckRequested;
 
@@ -88,9 +88,19 @@ namespace Surrender_20.UWP.View
             ChangeThemeLogo();
         }
 
-        private async void ChangeSite_Tapped(object sender, TappedRoutedEventArgs e)
+        private void Switch_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            await SiteChooseDialog.ShowAsync();
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+
+        private void Settings_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+
+        private async void AndroidApp_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            _ = await Launcher.LaunchUriAsync(new Uri(@"https://play.google.com/store/apps/details?id=com.versatilesofware"));
         }
 
         private void ChangeThemeLogo()
@@ -105,46 +115,5 @@ namespace Surrender_20.UWP.View
                 LogoImage.Source = LogoDark;
             }
         }
-
-        /*private void NavView_ItemSelected(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
-        {
-            NavigationViewItem item = args.SelectedItem as NavigationViewItem;
-
-            if (item.Content.Equals("Home"))
-            {
-                ContentFrame.Navigate(typeof(NewsfeedListView), null, new DrillInNavigationTransitionInfo());
-                //TitlePageTextBlock.Text = "Statystyki postaci";
-            }
-            else if (item.Content.Equals("PBE"))
-            {
-                ContentFrame.Navigate(typeof(NewsfeedListViewModel), null, new DrillInNavigationTransitionInfo());
-                //TitlePageTextBlock.Text = "Siła czarów i run";
-            }
-            else if (item.Content.Equals("Releases"))
-            {
-                ContentFrame.Navigate(typeof(NewsfeedListViewModel), null, new DrillInNavigationTransitionInfo());
-                //TitlePageTextBlock.Text = "Umiejętności";
-            }
-            else if (item.Content.Equals("Red Posts"))
-            {
-                ContentFrame.Navigate(typeof(NewsfeedListViewModel), null, new DrillInNavigationTransitionInfo());
-                //TitlePageTextBlock.Text = "Lista przedmiotów";
-            }
-            else if (item.Content.Equals("Rotations"))
-            {
-                ContentFrame.Navigate(typeof(NewsfeedListViewModel), null, new DrillInNavigationTransitionInfo());
-                //TitlePageTextBlock.Text = "Poradniki Youtube";
-            }
-            else if (item.Content.Equals("E-Sports"))
-            {
-                ContentFrame.Navigate(typeof(NewsfeedListViewModel), null, new DrillInNavigationTransitionInfo());
-                //TitlePageTextBlock.Text = "Poradniki Youtube";
-            }
-            else if (item.Content.Equals("Settings"))
-            {
-                ContentFrame.Navigate(typeof(SettingsViewModel), null, new DrillInNavigationTransitionInfo());
-                //TitlePageTextBlock.Text = "Informacje";
-            }
-        }*/
     }
 }
