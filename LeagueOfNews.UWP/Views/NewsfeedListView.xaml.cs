3f6128c2f23ef1006bee215f0b3bd91356dfb7d9
kcrg <kacper@tryniecki.com>
2019-04-19 21:48:03 +0200

    [UWP] ContextMenu with copying link to clipboard and sharing post, removed useless nuget  [Android] Fixed ads

--- LeagueOfNews.UWP/Views/NewsfeedListView.xaml.cs
+++ LeagueOfNews.UWP/Views/NewsfeedListView.xaml.cs
@@ -1,7 +1,13 @@
-﻿using LeagueOfNews.UWP.ViewModels;
+﻿using LeagueOfNews.Model;
+using LeagueOfNews.UWP.ViewModels;
 using LeagueOfNews.UWP.Views.Custom;
 using MvvmCross;
 using MvvmCross.IoC;
+using System;
+using System.IO;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 
@@ -12,6 +18,8 @@ namespace LeagueOfNews.UWP.View
 
     public sealed partial class NewsfeedListView : NewsfeedListViewBase
     {
+        public Newsfeed newsfeed = new Newsfeed();
+
         public NewsfeedListView()
         {
             InitializeComponent();
@@ -19,11 +27,51 @@ namespace LeagueOfNews.UWP.View
             //Workaround: MvxUserControl is custom-made, thus VM is not created by default
             ViewModel = MvxIoCProvider.Instance.IoCConstruct<NewsfeedListViewModel>();
             Mvx.IoCProvider.RegisterSingleton(ViewModel);
+
+            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
+            dataTransferManager.DataRequested += DataTransferManager_DataRequested;
+
+            if (DataTransferManager.IsSupported())
+            {
+                //Do sharing
+            }
+            else
+            {
+                //SharePost.Visibility = Visibility.Collapsed;
+                //CopyLink.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void GridView_Tapped(object sender, TappedRoutedEventArgs e)
         {
             ViewModel.ItemSelectedCommand.Execute((sender as GridView).SelectedItem);
+        }
+
+        private void CopyLink_Click(object sender, RoutedEventArgs e)
+        {
+            newsfeed = (Newsfeed)(sender as MenuFlyoutItem).DataContext;
+            DataPackage dataPackage = new DataPackage();
+            dataPackage.SetText(newsfeed.UrlToNewsfeed);
+            Clipboard.SetContent(dataPackage);
+        }
+
+        private void SharePost_Click(object sender, RoutedEventArgs e)
+        {
+            newsfeed = (Newsfeed)(sender as MenuFlyoutItem).DataContext;
+            DataTransferManager.ShowShareUI();
+        }
+
+        private void DataTransferManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            DataRequest request = args.Request;
+
+            request.Data.Properties.Title = newsfeed.Title;
+            request.Data.Properties.Description = newsfeed.ShortDescription;
+
+            MemoryStream stream = new MemoryStream(newsfeed.Image);
+            request.Data.Properties.Thumbnail = RandomAccessStreamReference.CreateFromStream(stream.AsRandomAccessStream());
+
+            request.Data.Properties.ContentSourceWebLink = new Uri(newsfeed.UrlToNewsfeed);
         }
     }
 }
